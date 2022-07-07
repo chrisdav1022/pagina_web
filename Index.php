18071b1f39910708aa807e2e8c16dfa6abf6766a
PatrciaGB <patitog01@gmail.com>
2022-07-07 10:42:38 -0500

    codigo inicial

---
+++ Index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TiendaOnline</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <ul class="nav navbar-nav">
+        <li class="nav-item">
+                <a class="nav-link" href="#">LOGO</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">HOME</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">ROPA</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">NOSOTROS</a>
+            </li>
+        </ul>
+    </nav>
+</body>
+</html>
+
