64167901363c4a253e4b721bcfad4a4b750b487a
Cristian Bedoya <chris_bedoya01@hotmail.com>
2022-07-07 11:32:04 -0500

    actual

---
+++ validar.php
@@ -0,0 +1,26 @@
+<?php
+$usuario=$_POST['usuario'];
+$contraseña=$_POST['contraseña'];
+session_start();
+$_SESSION['usuario']=$usuario;
+
+$conexion=mysqli_connect("localhost","root","nueva_pagina");
+
+$consulta="SELECT * FROM login WHERE login='$usuario' and contraseña='$contraseña'";
+$resultado=mysqli_query($conexion,$consulta);
+
+$filas=mysqli_num_rows($resultado);
+
+if($filas){
+    header("location:home.php");
+}
+else{
+    ?>
+    <?php
+    include("index.php")
+    ?>
+    <h1 class="bad">ERROR AL IDENTIFICARSE</h1>
+    <?php
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
